6f26ba335a18e487518838919ba7048fe140ece0
Zarafa Hudson auto-builder <hudson@zarafa.com>
2011-10-06 18:11:16 +0200

    Committing svn revision 29710

--- php-webclient-ajax/server/modules/class.listmodule.php
+++ php-webclient-ajax/server/modules/class.listmodule.php
@@ -197,6 +197,14 @@
 							$result = $this->copy($store, $parententryid, $entryid, $action);
 							break;
                         case "cancelInvitation":
+							$accessToFolder = false;
+							// Find the parententryid using the store and the entryid when it has not been sent in the request
+							if($store && $entryid && !$parententryid){
+								$folder = mapi_msgstore_openentry($store, $entryid);
+								$folderProps = mapi_getprops($folder, Array(PR_PARENT_ENTRYID));
+								$parententryid = $folderProps[PR_PARENT_ENTRYID];
+							}
+
 							if($store && $parententryid)
 								$accessToFolder = $GLOBALS["operations"]->checkFolderAccess($store, $parententryid);
 
