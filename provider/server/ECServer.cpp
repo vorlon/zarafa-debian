d8b5ed625c62d73db3b8ba787c2fc7e4d0305542
Zarafa Hudson auto-builder <hudson@zarafa.com>
2011-11-03 16:48:13 +0100

    Committing svn revision 30292

--- provider/server/ECServer.cpp
+++ provider/server/ECServer.cpp
@@ -1151,11 +1151,12 @@ int running_server(char *szName, const char *szConfig)
 	if (restart_searches) // restart_searches if specified
 		g_lpSessionManager->GetSearchFolders()->RestartSearches();
 
+	 g_Quit = 0;
 
 	// Create scheduler system
 	g_lpScheduler = new ECScheduler(g_lpLogger);
 	// Add a task on the scheduler
-	g_lpScheduler->AddSchedule(SCHEDULE_HOUR, 00, &SoftDeleteRemover);
+	g_lpScheduler->AddSchedule(SCHEDULE_HOUR, 00, &SoftDeleteRemover, (void*)&g_Quit);
 
 	g_lpScheduler->AddSchedule(SCHEDULE_HOUR, 15, &CleanupSyncsTable);
 	g_lpScheduler->AddSchedule(SCHEDULE_HOUR, 16, &CleanupSyncedMessagesTable);
@@ -1172,8 +1173,6 @@ int running_server(char *szName, const char *szConfig)
 	// high loglevel to always see when server is started.
 	g_lpLogger->Log(EC_LOGLEVEL_FATAL, "Startup succeeded on pid %d", getpid() );
 	g_lpStatsCollector->SetTime(SCN_SERVER_STARTTIME, time(NULL));
-
-	g_Quit = 0;
 
 	// Enter main accept loop
 	while(!g_Quit) {
@@ -1200,6 +1199,8 @@ exit:
 
 	delete g_lpSoapServerConn;
 
+	delete g_lpScheduler;
+
 	delete lpLicense;
 	if(st.ss_sp)
 		free(st.ss_sp);
@@ -1207,7 +1208,6 @@ exit:
 	delete lpDatabaseFactory;
 
 	delete g_lpConfig;
-	delete g_lpScheduler;
 
 	zarafa_exit();
 
