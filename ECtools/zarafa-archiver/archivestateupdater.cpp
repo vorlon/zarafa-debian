8ef1747c1e61625286e072f6580d6433c065e30a
marks <marks@zarafa.com>
2011-11-30 16:48:42 +0100

    Committing svn revision 30823: For ARCH-190: Add more logging when attaching and detaching stores.

--- ECtools/zarafa-archiver/archivestateupdater.cpp
+++ ECtools/zarafa-archiver/archivestateupdater.cpp
@@ -280,6 +280,7 @@ HRESULT ArchiveStateUpdater::RemoveImplicit(const entryid_t &storeId, const tstr
 	MsgStorePtr ptrUserStore;
 	StoreHelperPtr ptrUserStoreHelper;
 	ObjectEntryList lstCurrentArchives;
+	ULONG ulDetachCount = 0;
 
 	m_lpLogger->Log(EC_LOGLEVEL_DEBUG, "Removing implicitly attached archives.");
 
@@ -366,6 +367,7 @@ HRESULT ArchiveStateUpdater::RemoveImplicit(const entryid_t &storeId, const tstr
 		if (attachType == ImplicitAttach) {
 			m_lpLogger->Log(EC_LOGLEVEL_DEBUG, "Archive was implicitly attached, detaching.");
 			lstCurrentArchives.remove_if(Predicates::SObjectEntry_equals_binary(*i));
+			ulDetachCount++;
 		} else
 			m_lpLogger->Log(EC_LOGLEVEL_DEBUG, "Archive was explicitly attached");
 	}
@@ -375,6 +377,9 @@ HRESULT ArchiveStateUpdater::RemoveImplicit(const entryid_t &storeId, const tstr
 		m_lpLogger->Log(EC_LOGLEVEL_ERROR, "Failed to set archive list, hr=0x%08x", hr);
 		goto exit;
 	}
+
+	if (ulDetachCount > 0)
+		m_lpLogger->Log(EC_LOGLEVEL_FATAL, "Auto detached %u archive(s) from '" TSTRING_PRINTF "'.", ulDetachCount, userName.c_str());
 
 	hr = ptrUserStoreHelper->UpdateSearchFolders();
 
