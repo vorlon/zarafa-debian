15f69a18c25681e6f6764110a21d1c0e28292d8a
marks <marks@zarafa.com>
2011-12-05 08:31:13 +0100

    Committing svn revision 30890: Merged [30716][30801][30845][30870]: For ARCH-109

--- ECtools/zarafa-archiver/archiver.cpp
+++ ECtools/zarafa-archiver/archiver.cpp
@@ -208,15 +208,24 @@ exit:
 HRESULT Archiver::CreateManage(LPMAPISESSION lpSession, ECLogger *lpLogger, const TCHAR *lpszUser, ArchiveManagePtr *lpptrManage)
 {
 	HRESULT hr = hrSuccess;
+	ECConfig *lpsConfig = NULL; 
 	SessionPtr ptrArchiverSession;
 
-	hr = Session::Create(MAPISessionPtr(lpSession, true), lpLogger, &ptrArchiverSession);
+	lpsConfig = ECConfig::Create(GetConfigDefaults());
+	if (!lpsConfig->LoadSettings(GetConfigPath())) {
+		// Just log warnings and errors and continue with default.
+		if (lpLogger)
+			LogConfigErrors(lpsConfig, lpLogger);
+	}
+
+	hr = Session::Create(MAPISessionPtr(lpSession, true), lpsConfig, lpLogger, &ptrArchiverSession);
 	if (hr != hrSuccess)
 		goto exit;
 
 	hr = ArchiveManageImpl::Create(ptrArchiverSession, NULL, lpszUser, lpLogger, lpptrManage);
 
 exit:
+	delete lpsConfig;
 	return hr;
 }
 
