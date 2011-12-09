1bb824dfa01d29c0bd680ca007f75189c0d9985a
john <john@zarafa.com>
2011-12-09 17:30:53 +0100

    Committing svn revision 31079: use correct function for 0 test, for ZCP-8666

--- gateway/IMAP.cpp
+++ gateway/IMAP.cpp
@@ -5217,7 +5217,7 @@ HRESULT IMAP::HrParseSeqSet(const string &strSeqSet, list<ULONG> &lstMails) {
 	ULONG ulMailnr;
 	ULONG ulBeginMailnr;
 
-	if(lstFolderMailEIDs.size() == 0) {
+	if(lstFolderMailEIDs.empty()) {
 		hr = MAPI_E_NOT_FOUND;
 		goto exit;
 	}
@@ -5709,7 +5709,7 @@ HRESULT IMAP::HrSearch(vector<string> &lstSearchCriteria, ULONG &ulStartCriteria
 	}
 
 	// no need to search in empty folders, won't find anything
-	if (lstFolderMailEIDs.size() == 0)
+	if (lstFolderMailEIDs.empty())
 		goto exit;
 
 	// don't search if only search for uid, sequence set, all, recent, new or old
