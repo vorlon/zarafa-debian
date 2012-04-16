da51aa26eeec1617b8c182d96cf285b018bdf31b
parag <parag@zarafa.com>
2012-04-16 16:57:45 +0200

    Committing svn revision 33539: For WA-2949  - Stopped propagation MAPI_E_NO_ACCESS error when opening MR.  - Handled MAPI_E_NO_ACCESS error when responding MR with correct display message.  - Shown MR buttons if senderIsUser, as sender can send MR to Boss' and check as secretary.  - Handled MAPI_E_NO_ACCESS error if we don't have permission on parent folder to access then don't send notification for it.

--- php-ext/include/mapi/class.meetingrequest.php
+++ php-ext/include/mapi/class.meetingrequest.php
@@ -197,6 +197,16 @@ class Meetingrequest {
 	}
 
 	/**
+	 * Function will return the current user store.
+	 * @param resource $store the current userstore.
+	 * 
+	 */
+	function getStore()
+	{
+		return $this->store;
+	}
+
+	/**
 	 * Sets the direct booking property. This is an alternative to the setting of the direct booking
 	 * property through the constructor. However, setting it in the constructor is prefered.
 	 * @param Boolean $directBookingSetting
